e988ce5331526d97e0b7456d40eb229543c8ff38
FruggyDK <Christiankaae34@gmail.com>
2020-03-23 23:38:29 +0100

    added bugs

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
 
 int main(int argc, char *args[]) {
-    std::cout << "Game is running..." << std::endl;
+    Game *game = new Game();
+
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIHGT);
+
+    while (game->IsRunning()) {
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+    }
+
+    game->Destroy();
+
     return 0;
 }
 
